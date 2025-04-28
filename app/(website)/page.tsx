2747c18fefe93a66d2add5319586d4492cda519d
MatheusKristman <kristman058@gmail.com>
2025-04-28 20:39:26 -0300

    adding header

---
+++ app/(website)/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components/global/header";
+
+const Home = () => {
+  return (
+    <div className="w-full">
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
---
+++ components/global/header.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import { CircleHelpIcon, LogIn } from "lucide-react";
+
+import { Button } from "../ui/button";
+
+export const Header = () => {
+  return (
+    <header className="w-full sm:px-16 sm:pt-5 lg:container lg:mx-auto">
+      <div className="w-full bg-primary p-6 rounded-b-[50px] flex items-center justify-between gap-4 sm:rounded-full">
+        <div className="relative w-[143px] h-[35px]">
+          <Image src="/logo.png" alt="Busconecta" fill className="object-contain object-left" />
+        </div>
+
+        <div className="flex items-center gap-6">
+          <Button variant="ghost" size="lg" className="hidden sm:block text-secondary">
+            <CircleHelpIcon size={24} strokeWidth={1.5} />
+          </Button>
+
+          <Button variant="header" size="lg" className="text-xl">
+            <span>Entrar</span>
+
+            <LogIn size={24} strokeWidth={1.5} />
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
